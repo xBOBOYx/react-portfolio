afe0d9f11a9d94b0c220229276c90c909b55ac0a
Anthony Zamora <84356810+xBOBOYx@users.noreply.github.com>
2021-10-25 16:54:02 -0700

    init commit w/ starter code from act.24

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import "tailwindcss/tailwind.css"
+
+const App = () => <PortfolioContainer />;
+
+export default App;
